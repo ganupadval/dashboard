7fd49d7350edf61f588241033387fcb19d69a5cd
ganupadval <ganesh26padval@gmail.com>
2024-09-04 10:44:52 +0530

    mod

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import Nav from '../components/Nav';
-import Bar from '../components/Bar';
-import Location from '../components/Location';
-import Products from '../components/Products';
-import CustomerDetails from '../components/CustomerDetails';
-import Trends from '../components/Trends';
+import React, { useEffect, useState } from "react";
+import Nav from "../components/Nav";
+import Bar from "../components/Bar";
+import Location from "../components/Location";
+import Products from "../components/Products";
+import CustomerDetails from "../components/CustomerDetails";
+import Trends from "../components/Trends";
 
 const Dashboard = () => {
   const [locations, setLocations] = useState([]);
@@ -13,45 +13,44 @@ const Dashboard = () => {
   const [customers, setCustomers] = useState([]);
 
   useEffect(() => {
-    fetch('http://127.0.0.1:8000/locations')
-      .then(response => response.json())
-      .then(data => {
-        console.log("Locations Data:", data); 
+    fetch("https://dashboard-backend-eight.vercel.app/locations")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Locations Data:", data);
         setLocations(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("There was an error fetching the locations data!", error);
       });
-  
-    fetch('http://127.0.0.1:8000/products')
-      .then(response => response.json())
-      .then(data => {
+
+    fetch("https://dashboard-backend-eight.vercel.app/products")
+      .then((response) => response.json())
+      .then((data) => {
         setProducts(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("There was an error fetching the products data!", error);
       });
-  
-    fetch('http://127.0.0.1:8000/sales_data')
-      .then(response => response.json())
-      .then(data => {
-        console.log("Sales Data:", data);  
+
+    fetch("https://dashboard-backend-eight.vercel.app/sales_data")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Sales Data:", data);
         setSalesData(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("There was an error fetching the sales data!", error);
       });
-  
-    fetch('http://127.0.0.1:8000/customers')
-      .then(response => response.json())
-      .then(data => {
+
+    fetch("https://dashboard-backend-eight.vercel.app/customers")
+      .then((response) => response.json())
+      .then((data) => {
         setCustomers(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("There was an error fetching the customers data!", error);
       });
   }, []);
-  
 
   return (
     <div className="min-h-screen bg-gray-100 p-10">
